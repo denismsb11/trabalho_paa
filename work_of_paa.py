d1b4edefa79ab3a1101951a16fde7c99495a6df5
Denis Menezes <deniz@bannet.com.br>
2019-09-23 16:05:49 -0300

    correcao

--- work_of_paa.py
+++ work_of_paa.py
@@ -7,19 +7,16 @@ lines_of_matrix = len(weight_of_item)
 capacity_of_bag = 23
 
 matrix = np.zeros((lines_of_matrix + 1, capacity_of_bag + 1))
-print(matrix.shape)
 
-for row in range(matrix.shape[0]-1):
+for row in range(matrix.shape[0]):
     for column in range(matrix.shape[1]):
-        print(row,column)
         if row == 0 or column == 0:
             matrix[row][column] = 0
         else:
-            print(value_of_item[row])
-            if value_of_item[row] <= column:
-                matrix[row][column] = value_of_item[row]
-                matrix[row][column] = matrix[row - 1][column - row] + value_of_item[row] 
-            if matrix[row][column - 1] > matrix[row][column]:
-                 matrix[row][column] = matrix[row][column - 1]
+            if weight_of_item[row - 1] <= column:
+                matrix[row][column] = value_of_item[row - 1]
+                matrix[row][column] = matrix[row][column] + matrix[row - 1][column - weight_of_item[row - 1]] 
+            if matrix[row - 1][column] > matrix[row][column]:
+                matrix[row][column] = matrix[row - 1][column]
 
 print(pd.DataFrame(matrix))
